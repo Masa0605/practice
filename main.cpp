7b97345f926ad8cb2edae6f66d70d70395957b57
MasahideHarada0605 <Masahide.HARADA@jpd.co.jp>
2022-09-07 14:07:05 +0900

    second commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main(void)
 {
+    int a = 0;
+
     cout << "HelloWorld" << endl;
+    
+    cin >> a;
+
+    cout << "it's " << a << " o'clock now!" << endl; 
 
     return 0;
 }
